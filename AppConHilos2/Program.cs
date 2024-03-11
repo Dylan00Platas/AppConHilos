590199070d01221b7f23f29b84423d316953a43d
Dylan00Platas <131178283+Dylan00Platas@users.noreply.github.com>
2024-03-11 12:31:42 -0600

    Add files via upload

---
+++ AppConHilos2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace AppConHilos2
+{
+    class Program
+    {
+        static void Print(object obj)
+        {
+            // Se debe asegurar que el token viene como parámetro
+            if (obj == null || !(obj is CancellationToken token))
+                return;
+
+            for (int i = 11; i < 20; i++)
+            {
+                // Agregue la verificación de cancelación del token
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine($"En la iteración {i}, la cancelación ha sido solicitada...");
+                    // Termina la operación for
+                    break;
+                }
+
+                Console.WriteLine($"Print thread: {i}");
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Cree un hilo secundario pasando un delegado ParameterizedThreadStart
+            Thread workerThread = new Thread(new ParameterizedThreadStart(Print));
+
+            // Le colocamos nombre al hilo
+            workerThread.Name = "Hilo de Print";
+
+            // Crear el token source.
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            // Iniciar hilo secundario, ahora le enviamos un token
+            workerThread.Start(cts.Token);
+
+            // Hilo principal: Imprime de 1 a 10 cada 0,2 segundos.
+            // El método Thread.Sleep es responsable de hacer que el hilo actual entre en suspensión 
+            // en milisegundos. Mientras duerme, un hilo no hace nada.
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Principal thread: {i}");
+                Thread.Sleep(200);
+            }
+
+            // Si el hilo sigue ejecutándose, lo cancelamos
+            if (workerThread.IsAlive)
+                cts.Cancel();
+        }
+    }
+}
---
+++ AppConHilos3/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+
+public class Persona
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public string Sexo { get; set; }
+
+    public Persona(string nombre, int edad, string sexo)
+    {
+        this.Nombre = nombre;
+        this.Edad = edad;
+        this.Sexo = sexo;
+    }
+}
+
+class Program
+{
+    static void TareaDeFondoConParametro(Object? stateInfo)
+    {
+        if (stateInfo == null)
+            return;
+
+        Thread currentThread = Thread.CurrentThread;
+        Console.WriteLine($"Es un thread background: {currentThread.IsBackground}");
+
+        Persona data = (Persona)stateInfo;
+        Console.WriteLine($"Hola {data.Nombre} desde ThreadPool.");
+        Thread.Sleep(1000);
+    }
+
+    static void TareaDeFondo(Object? stateInfo)
+    {
+        Console.WriteLine($"Hilo 1: Hola soy un hilo sin uso parámetros desde ThreadPool.");
+        Thread.Sleep(1500);
+    }
+
+    static void Main(string[] args)
+    {
+        int workers, ports;
+
+        // Obtener el máximo número de hilos
+        ThreadPool.GetMaxThreads(out workers, out ports);
+        Console.WriteLine($"Máximos hilos de trabajo: {workers}");
+        Console.WriteLine($"Máximos puertos para hilos: {ports}");
+
+        // Obtener un número mínimo de hilos
+        ThreadPool.GetMinThreads(out workers, out ports);
+        Console.WriteLine($"Mínimos hilos de trabajo: {workers}");
+        Console.WriteLine($"Mínimos puertos (completion port) para hilos: {ports}");
+
+        // Obtener hilos disponibles
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles: {workers}");
+        Console.WriteLine($"Hilos de puerto (completion port) disponibles: {ports}");
+
+        // Obtenga el número total de procesadores disponibles en la máquina
+        int processCount = Environment.ProcessorCount;
+        Console.WriteLine($"No. de procesadores disponibles en el sistema: {processCount}");
+        Console.WriteLine($"---------------------------------");
+
+        // Utilice ThreadPool para un hilo de trabajo
+        ThreadPool.QueueUserWorkItem(TareaDeFondo);
+
+        // Obtener hilos disponibles
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles después del hilo 1: {workers}");
+
+        // Crea un objeto y lo envía al hilo de trabajo de ThreadPool
+        Persona p = new Persona("Dylan Platas", 21, "Hombre");
+        ThreadPool.QueueUserWorkItem(TareaDeFondoConParametro, p);
+
+        // Obtener hilos disponibles
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles después del hilo 2: {workers}");
+
+        // Esperamos a que ambos hilos finalicen
+        Thread.Sleep(2000);
+
+        // Obtener hilos disponibles
+        ThreadPool.GetAvailableThreads(out workers, out ports);
+        Console.WriteLine($"Hilos de trabajo disponibles al final: {workers}");
+
+        // Esta instrucción es necesaria para que no termine el programa
+        Console.ReadKey();
+    }
+}
